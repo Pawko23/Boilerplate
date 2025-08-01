26d81d6bed5566a072e2828091b4abc76846e2fc
aleksander <aleksander.pawlak@op.pl>
2025-08-01 08:27:57 +0200

    Add shrek icon in base64

--- src/js/employeeList.js
+++ src/js/employeeList.js
@@ -1,6 +1,6 @@
-const input = document.getElementById('employeeInput');
-const button = document.getElementById('addButton');
-const list = document.getElementById('employeeList');
+const employeeInput = document.getElementById('employeeInput');
+const addButton = document.getElementById('addButton');
+const employeeList = document.getElementById('employeeList');
 
 const employees = [];
 
@@ -10,18 +10,19 @@ const addEmployee = (name) => {
     const li = document.createElement('li');
     li.textContent = name;
     li.classList.add('employee-container__list-item');
-    list.appendChild(li);
+    employeeList.appendChild(li);
 
-    input.value = '';
-    input.focus();
+    employeeInput.value = '';
+    employeeInput.focus();
 };
 
-button.addEventListener('click', () => {
-    addEmployee(input.value);
+addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    addEmployee(employeeInput.value);
 });
 
-input.addEventListener('keypress', (e) => {
+employeeInput.addEventListener('keypress', (e) => {
     if(e.key === 'Enter') {
-        addEmployee(input.value);
+        addEmployee(employeeInput.value);
     }
 });
