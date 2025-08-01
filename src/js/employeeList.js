e8af43da31452a38d7951b59eae9da8a7bdaf522
aleksander <aleksander.pawlak@op.pl>
2025-08-01 11:06:48 +0200

    Add handling of form default behavior

--- src/js/employeeList.js
+++ src/js/employeeList.js
@@ -1,28 +1,35 @@
-const employeeInput = document.getElementById('employeeInput');
-const addButton = document.getElementById('addButton');
-const employeeList = document.getElementById('employeeList');
+document.addEventListener('DOMContentLoaded', () => {
+    const employeeInput = document.getElementById('employeeInput');
+    const employeeForm = document.getElementById('employeeForm');
+    const addButton = document.getElementById('addButton');
+    const employeeList = document.getElementById('employeeList');
+    const employees = [];
 
-const employees = [];
+    employeeForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
 
-const addEmployee = (name) => {
-    employees.push(name);
+    const addEmployee = (name) => {
+        employees.push(name);
 
-    const li = document.createElement('li');
-    li.textContent = name;
-    li.classList.add('employee-container__list-item');
-    employeeList.appendChild(li);
+        const li = document.createElement('li');
+        li.textContent = name;
+        li.classList.add('employee-container__list-item');
+        employeeList.appendChild(li);
 
-    employeeInput.value = '';
-    employeeInput.focus();
-};
+        employeeInput.value = '';
+        employeeInput.focus();
+    };
 
-addButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    addEmployee(employeeInput.value);
-});
-
-employeeInput.addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
+    addButton.addEventListener('click', (e) => {
+        e.preventDefault();
         addEmployee(employeeInput.value);
-    }
+    });
+
+    employeeInput.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            e.preventDefault();
+            addEmployee(employeeInput.value);
+        }
+    });
 });
